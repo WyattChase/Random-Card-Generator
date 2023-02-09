1599cac18035b422b0e50b8f1941541bd1840711
WyattChase <esnaruto@yahoo.com>
2023-02-09 09:40:20 +0000

    update

--- src/app.js
+++ src/app.js
@@ -7,15 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  var person = {
-    first: "Jean",
-    last: "St. Cloud",
-    age: 30
-  };
-  var htmlElement = `<h1> Hello ${person.first} ${person.last} you are ${person.age} years old</h1>`;
-  console.log(htmlElement);
-
-  document.getElementById("hello").innerHTML = htmlElement;
-
-  console.log();
+  document.querySelector(".card").innerHTM = random_suit();
+  document.querySelector(".card").innerHTML = random_number();
 };
+
+//Suits and Values = Array
+let random_suit = () => {
+  let suit = ["♠", "♥", "♣", "♦"];
+  let random_suit = suit[Math.floor(Math.random() * suit.length)];
+  return suit[random_suit];
+};
+
+let random_number = () => {
+  let number = ["2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  let random_number = number[Math.floor(Math.random() * number.length)];
+  return number[random_number];
+};
+//randomly get one suit and value
+
+//'Create the html elemet' '<h1></h1>'p
+
+//appl the suit to indexhtml
